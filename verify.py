481e7048dc71eb5de58f3a6fafeddba1bb55ef32
laralmyan <laralmyan@gmail.com>
2025-10-16 20:33:15 +0400

    Feat: Complete data analysis for session 2

---
+++ verify.py
@@ -0,0 +1,6 @@
+import platform
+
+
+print("Congratulations! Your local forge is operational.") 
+print(f"Python Version: {platform.python_version()}") 
+print(f"Operating System: {platform.system()} {platform.release()}")
